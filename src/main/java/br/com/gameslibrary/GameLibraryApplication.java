b3403952a0f53fa07a587a7a5e3f88482bce4be2
larissa olimpio <larissa.olimpio@alura.com.br>
2024-09-03 10:23:57 -0300

    First commit: Start project

---
+++ src/main/java/br/com/gameslibrary/GameLibraryApplication.java
@@ -0,0 +1,20 @@
+package br.com.gameslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class GameLibraryApplication {
+
+    @GetMapping("/dashboard")
+    public String oi() {
+        return "dashboard";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameLibraryApplication.class, args);
+    }
+}
